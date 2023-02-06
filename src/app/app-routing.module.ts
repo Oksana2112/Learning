738d22225badf3d921fb3b37cfdd8e8fcd3d32dc
Oksana2112 <oksanakr346@gmail.com>
2023-02-06 18:27:49 +0100

    completed game play - profile screen

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { GameProfileComponent } from './components/game-profile/game-profile.component';
 import { HomeComponent } from './components/home/home.component';
+import { MedCardComponent } from './components/med-card/med-card.component';
 
 
 
-const routes: Routes = [{ path: 'game-profile', component: GameProfileComponent }, { path: '', component:HomeComponent,pathMatch:'full'}];
+const routes: Routes = [{ path: 'game-profile', component: GameProfileComponent },{ path: 'med-card', component: MedCardComponent },{ path: '', component:HomeComponent,pathMatch:'full'}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
